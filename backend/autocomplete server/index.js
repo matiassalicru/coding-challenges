a12f722a6083161853941f8df485d6ad9886819b
Matias Salicru <mattiassalicru@gmail.com>
2021-05-01 08:27:47 -0300

    Autocomplete in a server

---
+++ backend/autocomplete server/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require ('cors');
+const app = express();
+
+app.use(cors());
+
+const port = 4000;
+
+var fs = require("fs");
+var dictionary = fs.readFileSync("/usr/share/dict/words").toString().split("\n");
+
+const autocomplete = (prefix) => {                  // O(1)
+  
+    const list = [];                                // O(1)
+    
+    // const lowerPrefix = prefix.toLowerCase();
+    
+    for(let i = 0; i < dictionary.length; i++){     // O(n)
+      //Add toLowerCase to remove caseSentitive
+      if(dictionary[i].startsWith(prefix)) {        // O(n)
+        list.push(dictionary[i]);                   // O(n)
+      }
+    }
+  return list                                       // O(1)
+}
+
+app.get('/prefix/:pre', (req, res) => {
+    const words = autocomplete(req.params.pre)
+    res.json(words);
+})
+
+app.listen(port, () => {
+    console.log(`Server on ; ${port}`);
+});
+
+
