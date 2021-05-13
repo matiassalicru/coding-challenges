8bbc084499594b832f1f61577ec1ec4fed8d2fbd
Matias Salicru <mattiassalicru@gmail.com>
2021-05-12 22:05:43 -0300

    added function print for multiple numbers, and added comments

--- FizzbuzzWithTDD/FizzBuzz.js
+++ FizzbuzzWithTDD/FizzBuzz.js
@@ -1,18 +1,30 @@
 const fizzbuzz = (num) => {
-    if ( num % 3 === 0 && num % 5 === 0) {
+
+    // If num is multiple of 5 and multiple of 3 return 'fizzbuzz'
+    if (num % 3 === 0 && num % 5 === 0) {
         return 'fizzbuzz';
     }
 
+    // If num is multiple of 3 return 'fizz'
     if (num % 3 === 0) {
         return 'fizz';
     }
 
+    // If num is multiple of 5 return 'buzz'
     if (num % 5 === 0) {
         return 'buzz';
     }
 
     return num;
-
 }
+
+// Function to print number from zero to the passed number executing fizzbuzz function.
+const print = (num) => {
+    for (let i = 0; i < num; i++) {
+        console.log(`${i}: ${fizzbuzz(i)}`);
+    }
+}
+
+print(16)
 
 module.exports = fizzbuzz;
